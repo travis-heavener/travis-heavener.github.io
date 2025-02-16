63b762756c63e5e698f438713d5965c98555ae55
Travis Heavener <travis.heavener@gmail.com>
2025-02-16 00:31:37 -0500

    Improved dropdown content UI

--- projects/index.js
+++ projects/index.js
@@ -1,27 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-    [...document.getElementsByClassName("section-wrapper")].forEach((wrapper, i) => {
-        // Hide projects row
-        const projectsRow = wrapper.nextElementSibling;
-        projectsRow.className = "projects-row hidden";
-
+    [...document.getElementsByClassName("section-wrapper")].forEach(wrapper => {
         // Bind click evts
+        const projectsWrapper = wrapper.nextElementSibling;
         const ANIM_DURATION = 150; // In ms, duration of projects row animation
         let timeout = null;
+        
         wrapper.addEventListener("click", function() {
             // Toggle class
             const icon = this.children[2];
             const isActive = !icon.className.includes("active");
             if (isActive) {
                 icon.classList.add("active");
-                projectsRow.classList.remove("hidden"); // Focus projects row
+                projectsWrapper.classList.remove("hidden"); // Focus projects row
 
                 // Trigger animation
                 if (timeout !== null)
                     clearTimeout(timeout);
 
-                projectsRow.classList.add("animate-in");
+                projectsWrapper.classList.add("animate-in");
                 timeout = setTimeout(() => {
-                    projectsRow.classList.remove("animate-in");
+                    projectsWrapper.classList.remove("animate-in");
                     timeout = null;
                 }, ANIM_DURATION);
             } else {
@@ -31,16 +29,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 if (timeout !== null)
                     clearTimeout(timeout);
                 
-                projectsRow.classList.add("animate-out");
+                projectsWrapper.classList.add("animate-out");
                 timeout = setTimeout(() => {
-                    projectsRow.classList.remove("animate-out");
-                    projectsRow.classList.add("hidden"); // Hide projects row
+                    projectsWrapper.classList.remove("animate-out");
+                    projectsWrapper.classList.add("hidden"); // Hide projects row
                     timeout = null;
                 }, ANIM_DURATION * 0.8); // Add buffer to prevent clipping
             }
         });
-
-        // If first dropdown area, focus
-        if (i === 0) wrapper.click();
     });
 });
