4876bfc15f3246c293363e836a7222768e8b1198
Travis Heavener <travis.heavener@gmail.com>
2025-10-25 04:33:16 -0400

    Now dynamically loads bio paragraphs

--- tools/load-bio.py
+++ tools/load-bio.py
@@ -7,10 +7,21 @@ if __name__ == "__main__":
     # CD to script directory
     os.chdir( os.path.dirname(os.path.abspath(__file__)) )
 
+    # Load json
+    data = None
+    with open("../data.json", "r") as f:
+        data = json.load(f)["bio"]
+
     # Update timestamp
     with open("../templates/bio/index.html", "r") as f:
         # Read file
         contents = f.read()
+
+        # Load paragraphs
+        contents = contents.replace("%%PARAGRAPH 1.1%%", data["paragraphs"][0][0])
+        contents = contents.replace("%%PARAGRAPH 1.2%%", data["paragraphs"][0][1])
+        contents = contents.replace("%%PARAGRAPH 2.1%%", data["paragraphs"][1][0])
+        contents = contents.replace("%%PARAGRAPH 2.2%%", data["paragraphs"][1][1])
 
         # Replace timestamp
         contents = contents.replace("%%TIMESTAMP%%", gen_timestamp(include_top_anchor=True))
