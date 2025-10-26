117a821acc21e97e0cd8342762e8ffd5011203a7
Travis Heavener <travis.heavener@gmail.com>
2025-10-26 17:20:52 -0400

    Abstracted ANSI color escape codes

--- tools/generate-home.py
+++ tools/generate-home.py
@@ -27,6 +27,9 @@ if __name__ == "__main__":
         # Replace timestamp
         contents = contents.replace("%%TIMESTAMP%%", pad_left(gen_timestamp_txt(), max_len=17))
 
+        # Escape shell colors
+        contents = escape_shell_colors(contents)
+
         # Write to new file
         with open("../docs/sh/home.txt", "w") as f:
             f.write(contents)
