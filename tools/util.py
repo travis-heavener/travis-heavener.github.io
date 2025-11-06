920870faa21fd40aa4a4b814117e27ebfcfcd74a
Travis Heavener <travis.heavener@gmail.com>
2025-11-06 01:28:19 -0500

    Streamline header classes

--- tools/util.py
+++ tools/util.py
@@ -44,10 +44,10 @@ def inject_header(body: str) -> str:
     return body.replace("%%HEADER%%",
         """
             <header>
-                <a id="home-anchor" class="header-anchor no-select" href="/">T</a>
-                <a class="header-anchor" href="/resume/">Resume</a>
-                <a class="header-anchor" href="/projects/">Projects</a>
-                <a class="header-anchor" href="/bio/">About</a>
+                <a id="home-anchor" class="no-select" href="/">T</a>
+                <a href="/resume/">Resume</a>
+                <a href="/projects/">Projects</a>
+                <a href="/bio/">About</a>
             </header>
         """
     )
