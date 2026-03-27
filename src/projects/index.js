4803975d474c95344efbfa37d2266dec7b01f5ec
Travis Heavener <travis.heavener@gmail.com>
2026-03-27 03:09:49 -0400

    Migrate to pure CSS dropdowns (#73) Closes #73

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,32 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-    for (const wrap of document.getElementsByTagName("section")) {
-        // Bind click evts
-        const projWrap = wrap.nextElementSibling;
-        let timeout;
-
-        const toggleFocus = function() {
-            // Toggle class
-            const icon = wrap.children[2];
-            if (!icon.className.includes("active")) {
-                icon.classList.add("active");
-                projWrap.classList.remove("hidden");
-                projWrap.classList.add("animate-in");
-
-                if (timeout) clearTimeout(timeout); // Trigger animation
-                timeout = setTimeout(() => projWrap.classList.remove("animate-in"), 150);
-            } else {
-                icon.classList.remove("active");
-                projWrap.classList.add("animate-out");
-
-                if (timeout) clearTimeout(timeout); // Trigger animation
-                timeout = setTimeout(() => {
-                    projWrap.classList.remove("animate-out");
-                    projWrap.classList.add("hidden");
-                }, 120);
-            }
-        };
-
-        wrap.addEventListener("click", e => (e.target === wrap || e.target === wrap.children[1] || e.target === wrap.children[2]) ? toggleFocus() : null);
-        wrap.children[2].addEventListener("keyup", e => e.code === "Enter" && toggleFocus());
+// If JS is enabled, remove location.hash when clicking on dropdown
+// to allow it to be minimized (since :target pseudo-class is stubborn)
+const catchHashChange = () => {
+    // Clear hash when toggling checkbox
+    if (location.hash) {
+        const checkbox = document.getElementById(location.hash.substring(1)).parentElement.parentElement.nextSibling;
+        checkbox.addEventListener("change", () => {
+            // Force close the checkbox content & remove hash
+            checkbox.checked = false;
+            location.hash = "";
+        }, { "once": true });
     }
-});
+};
+
+// Bind event listeners
+window.addEventListener("hashchange", catchHashChange);
+document.addEventListener("DOMContentLoaded", catchHashChange);
